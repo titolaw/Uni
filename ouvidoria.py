dc4fe739ac0958c539807ac2e750be54688b4a21
titolaw <93658150+titolaw@users.noreply.github.com>
2023-03-06 19:20:53 -0300

    nv

--- ouvidoria.py
+++ ouvidoria.py
@@ -3,9 +3,9 @@ opcao = 10
 while opcao != 5:
 
     # Listar reclamacoes
-    print()
+    print("========== MENU ==========")
     print("1) Listar reclamações")
-    print("2) Adcionar nova reclamação")
+    print("2) Adicionar nova reclamação")
     print("3) Remover uma reclamação")
     print("4) Pesquisar uma reclamação por código")
     print("5) Sair")
@@ -16,10 +16,14 @@ while opcao != 5:
     if opcao == 1:
         if len(lista) == 0:
             print()
-            print("Nenhuma reclamação cadastrada no sistema")
+            print(" - Nenhuma reclamação cadastrada no sistema")
+            print()
         else:
+            print("===== LISTA DE RECLAMAÇÕES =====")
+            print()
             for index, item in enumerate(lista):
                 print(f"{index+1}) {item}")
+            print()
 
     # Adcionar novas reclamações
     elif opcao == 2:
@@ -28,6 +32,7 @@ while opcao != 5:
         print()
         print("Reclamação cadastrada com sucesso")
         print(f"O código da reclamação é {len(lista)}")
+        print()
 
     # Remover uma reclamação
     elif opcao == 3:
@@ -39,19 +44,23 @@ while opcao != 5:
             print("Código inválido")
         else:
             lista.pop(remocao - 1)
+            print()
 
     # Pesquisar uma reclamação por código
     elif opcao == 4:
         pesquisa = int(input("Digite o número da reclamação a ser pesquisada: "))
         if pesquisa <= 0 or pesquisa > len(lista):
-            print()
             print("Código inválido")
+            print()
         else:
             print()
-            print(lista[pesquisa - 1])
+            print(f"a reclamação pesquisada é: {lista[pesquisa - 1]}")
+            print()
 
     # Opção inválida e saída
     elif opcao != 5:
+        print()
         print("Opção inválida")
+        print()
 
-print("Adeus")
+print("sair")
