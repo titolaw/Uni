716ae4a64c44c288322154a805d033bfaa5172c3
titolaw <93658150+titolaw@users.noreply.github.com>
2023-04-10 11:33:10 -0300

    P3

---
+++ ouvidoria/main.py
@@ -0,0 +1,47 @@
+from ouvidoria import *
+from operacoes import *
+
+conexao = abrirConexao()
+
+opcao = 10
+while opcao != 5:
+
+    opcao = menu()
+
+    if opcao == 1:
+        print("===== LISTA DE RECLAMAÇÕES =====")
+        print()
+        reclamacoes = lista(conexao)
+        print(reclamacoes)
+        print()
+
+    elif opcao == 2:
+        reclamacao = input("Digite a sua reclamacao: ")
+        print()
+        inserirReclamacao(conexao, reclamacao)
+        print('A reclamação foi inserida com sucesso!')
+        print()
+
+    elif opcao == 3:
+        excluir = input("Digite o código da reclamação a ser removida: ")
+        print()
+        remocao = removerReclamacao(conexao, excluir)
+        print(remocao)
+        print()
+
+    elif opcao == 4:
+        codigo = input("Digite o código da reclamação a ser pesquisada: ")
+        print()
+        reclamacaoPorCodigo = pesquisarReclamacao(conexao, codigo)
+        print(reclamacaoPorCodigo)
+        print()
+
+    elif opcao != 5:
+        print()
+        print("Opção inválida")
+        print()
+
+print()
+print("Até logo. Esperamos ter ajudado.")
+
+encerrarBancoDados(conexao)
---
+++ ouvidoria/ouvidoria.py
@@ -0,0 +1,63 @@
+from operacoes import *
+
+def abrirConexao():
+    conexao = abrirBancoDados('localhost', 'root', 'root', 'ouvidoria')
+    return conexao
+
+def menu():
+    # Listar reclamacoes
+    print("========== MENU ==========")
+    print("1) Listar reclamações")
+    print("2) Adicionar nova reclamação")
+    print("3) Remover uma reclamação por código")
+    print("4) Pesquisar uma reclamação por código")
+    print("5) Sair")
+    print()
+    opcao = int(input("Bem vindo à ouvidoria. Escolha o número da sua opção: "))
+    return opcao
+
+def lista(conexao):
+    # checa a existência de dodos no banco e os lista
+    consultaReclamacao = 'select * from reclamacao'
+    listaReclamacao = listarBancoDados(conexao, consultaReclamacao)
+
+    if len(listaReclamacao) == 0:
+        return " - Nenhuma reclamação cadastrada no sistema."
+    else:
+        reclame = ""
+        for reclamacao in listaReclamacao:
+            reclame += f"codigo: {reclamacao[0]} | reclamação: {reclamacao[1]}\n"
+        return reclame
+
+
+def inserirReclamacao(conexao, reclamacao):
+    # insere dados no banco
+    inserirReclamacao = 'insert into reclamacao(titulo) values(%s)'
+    dados = (reclamacao,)
+    insertNoBancoDados(conexao, inserirReclamacao, dados)
+
+def removerReclamacao(conexao, excluir):
+    # sql a ser utilizado para a exclusão do dado
+    deletaCodigo = 'delete from reclamacao where id = %s'
+    dados = (excluir,)
+
+    # retorna uma lista para checar se o dado a ser excluido existe no banco
+    consultaCodigo = 'select * from reclamacao where id = ' + excluir
+    listaCodigo = listarBancoDados(conexao, consultaCodigo)
+
+    if len(listaCodigo) == 0:
+        return "Não existe reclamação com esse código."
+    else:
+        excluirBancoDados(conexao, deletaCodigo, dados)
+        return "Reclamação removida com sucesso."
+
+def pesquisarReclamacao(conexao, codigo):
+    # sql para pesquisar um item no banco de dados
+    consultaCodigo = 'select * from reclamacao where id = ' + codigo
+    listaCodigo = listarBancoDados(conexao, consultaCodigo)
+
+    if len(listaCodigo) == 0:
+        return "Não existe reclamação com esse código."
+    else:
+        for reclamacao in listaCodigo:
+            return f"codigo: {reclamacao[0]} | reclamação: {reclamacao[1]}"
