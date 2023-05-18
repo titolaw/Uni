3de9b7cc4e93239accc1634db61131c6f18e0eb5
tito <aquilala@gmail.com>
2023-05-18 17:11:10 -0300

    12

--- exercicio/Java1.java
+++ exercicio/Java1.java
@@ -1,86 +1,54 @@
- 
 package application;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Ouvidoria {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void menu() {
 
-//		1
-//		System.out.println("Digite o raio da circunferência: ");
-//		int raio = sc.nextInt();
-//		System.out.println("O diâmetro do círculo é: " + raio * 2);
-//		System.out.println("O comprimento do círculo é: " + raio * 6.28);
-//		System.out.println("A área do círculo é: " + (raio * raio * 3.14));
-
-//		2
-//		System.out.println("Digite o primeiro valor: ");
-//		int valor1 = sc.nextInt();
-//		System.out.println("Digite o segundo valor: ");
-//		int valor2 = sc.nextInt();
-//		System.out.println("Digite o terceiro valor: ");
-//		int valor3 = sc.nextInt();
-//
-//		if (valor1 > valor2 && valor1 > valor3) {
-//			System.out.println("O primeiro valor é o maior");
-//		}
-//		else if (valor2 > valor1 && valor2 > valor3) {
-//			System.out.println("O segundo valor é o maior");
-//		}
-//		else {
-//			System.out.println("O terceiro valor é o maior");
-//		}
-
-//		3
-//		System.out.println("Digite um valor: ");
-//		int valor = sc.nextInt();
-//		if (valor % 2 == 0)
-//			System.out.println("O valor é par");
-//		else
-//			System.out.println("O valor é impar");
-
-//		5
-//		System.out.println("Digite um nota: ");
-//		int nota = sc.nextInt();
-//		if (nota >= 7 )
-//			System.out.println("Passou direto");
-//		else if (nota >= 5 && nota < 7)
-//			System.out.println("Tem direito de fazer uma prova de recuperação");
-//		else
-//			System.out.println("Reprovado direto");
-
-//		6
-//		Random randomGenerator = new Random();
-//		int num_aleatorio = randomGenerator.nextInt(10) + 1;
-//		System.out.println(num_aleatorio);
-//		for(int i = 1; i < 11; i++)
-//			if (i == num_aleatorio)
-//				System.out.println("O numero aleatório é " + num_aleatorio);
-
-//		13
-//		int senha = 1234;
-//		System.out.print("Digite a senha: ");
-//		int guess = sc.nextInt();
-//		if (guess == senha)
-//			System.out.println("ACESSO PERMITIDO");
-//		else
-//			System.out.println("ACESSO NEGADO");
-
-//		14
-//		System.out.print("Digite o número de maçãs a serem compradas: ");
-//		int macas = sc.nextInt();
-//		double valorCompra;
-//		if (macas >= 12)
-//			valorCompra = macas * 0.25;
-//		else
-//			valorCompra = macas * 0.30;
-//		System.out.println("O valor da compra é " + valorCompra);
-
-//		15
+		System.out.print("|--------------------------------|\n");
+		System.out.print("| Opção 1 - Inserir reclamações  |\n");
+		System.out.print("| Opção 2 - Listar reclamações   |\n");
+		System.out.print("| Opção 3 - Sair                 |\n");
+		System.out.print("|--------------------------------|\n");
 
 	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] ocorrencias = new String[2];
+
+		menu();
+		int opcao = 9;
+
+		while (opcao != 3) {
+			System.out.print("Digite uma opção: ");
+			opcao = sc.nextInt();
+			switch (opcao) {
+			case 1:
+				System.out.println("Inserir reclamações");
+				for(int x = 0; x < ocorrencias.length;x++) {
+					System.out.println("Digite uma reclamação: ");
+					ocorrencias[x] = sc.nextLine();
+					sc.next();
+				}
+				break;
+
+			case 2:
+				System.out.println("Listar reclamações");
+				for(String item : ocorrencias) {
+					System.out.println(item);
+				}
+				break;
+
+			case 3:
+				System.out.println("Sair. Até logo!");
+				break;
+			default:
+				System.out.println("Opção inválida");
+			}
+
+		}
+		sc.close();
+	}
 }
